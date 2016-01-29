46d260bc08754191deaf6ea714a7ff7d08acfb7b
Atsushi Yamazaki <atsushi_yamazaki@relationsgroup.co.jp>
2016-01-29 17:54:59 +0900

    del dust

--- subnets.tf
+++ subnets.tf
@@ -6,28 +6,12 @@ resource "aws_subnet" "public-a" {
         Name = "public-a"
     }
 }
-resource "aws_subnet" "private-a" {
-    vpc_id = "${aws_vpc.opsworks_vpc.id}"
-    cidr_block = "192.168.15.0/24"
-    availability_zone = "ap-northeast-1a"
-    tags {
-        Name = "private-a"
-    }
-}
 resource "aws_subnet" "public-c" {
     vpc_id = "${aws_vpc.opsworks_vpc.id}"
     cidr_block = "192.168.20.0/24"
     availability_zone = "ap-northeast-1c"
     tags {
         Name = "public-c"
-    }
-}
-resource "aws_subnet" "private-c" {
-    vpc_id = "${aws_vpc.opsworks_vpc.id}"
-    cidr_block = "192.168.25.0/24"
-    availability_zone = "ap-northeast-1c"
-    tags {
-        Name = "private-c"
     }
 }
 
@@ -38,16 +22,4 @@ resource "aws_route_table_association" "puclic-a" {
 resource "aws_route_table_association" "puclic-c" {
     subnet_id = "${aws_subnet.public-c.id}"
     route_table_id = "${aws_route_table.public-route.id}"
-}
-
-resource "aws_db_subnet_group" "subnet-rds" {
-    name = "subnet-rds"
-    description = "subnet rds"
-    subnet_ids = [
-        "${aws_subnet.private-a.id}",
-        "${aws_subnet.private-c.id}",
-    ]
-    tags {
-        Name = "subnet-rds"
-    }
 }
